2f48068c99be7acc04000e2f1ea027301b464fe0
Mehmet Erdem AKIN <52917167+Mehmet-Erdem-Akin@users.noreply.github.com>
2020-10-06 22:25:58 +0300

    first upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function dunya() {
+    document.getElementById("gezegen").src = "pngegg (7).png";
+
+}
+
+function gunes() {
+    document.getElementById("gezegen").src = "pngegg (10).png";
+
+}
+
+function mars() {
+    document.getElementById("gezegen").src = "pngegg (9).png";
+
+}
+
+function saturn() {
+    document.getElementById("gezegen").src = "pngegg (15).png";
+
+}
